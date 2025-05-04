bad354fa11b0fadc37a2a831bf9118032370e7a1
Saverio Negro <saverionegro@gmail.com>
2025-05-04 16:51:22 +0200

    Initial Commit

---
+++ SpacePatrol/SpacePatrolApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpacePatrolApp.swift
+//  SpacePatrol
+//
+//  Created by Saverio Negro on 04/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SpacePatrolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
